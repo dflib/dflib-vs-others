04375e51f988bfd4651469b7f9d1d23c5c41a5a8
Andrus Adamchik <andrus@objectstyle.com>
2024-09-28 14:03:16 -0400

    dataframe-ec

---
+++ dataframe-ec/src/main/java/org/dflib/dataframeec/CalculateAverage.java
@@ -0,0 +1,48 @@
+package org.dflib.dataframeec;
+
+import io.github.vmzakharov.ecdataframe.dataframe.DataFrame;
+import io.github.vmzakharov.ecdataframe.dataset.CsvDataSet;
+import io.github.vmzakharov.ecdataframe.dataset.CsvSchema;
+import org.eclipse.collections.api.factory.Lists;
+
+import java.io.File;
+import java.net.URI;
+import java.nio.file.Path;
+
+import static io.github.vmzakharov.ecdataframe.dataframe.AggregateFunction.*;
+import static io.github.vmzakharov.ecdataframe.dsl.value.ValueType.FLOAT;
+import static io.github.vmzakharov.ecdataframe.dsl.value.ValueType.STRING;
+
+public class CalculateAverage {
+
+    static private final String FILE = "measurements.txt";
+
+    public static void main(String[] args) {
+
+        URI measurementFile = new File(FILE).toURI();
+
+        CsvSchema msSchema = new CsvSchema()
+                .addColumn("Station", STRING)
+                .addColumn("Temperature", FLOAT)
+                .separator(';')
+                .hasHeaderLine(false);
+
+        CsvDataSet msDataSet = new CsvDataSet(Path.of(measurementFile), "measurements", msSchema);
+
+        DataFrame measurements = msDataSet.loadAsDataFrame();
+
+        DataFrame aggregated = measurements
+                .aggregateBy(
+                        Lists.immutable.of(min("Temperature", "Min"), avg2d("Temperature", "Mean"), max("Temperature", "Max")),
+                        Lists.immutable.of("Station"))
+                .sortBy(Lists.immutable.of("Station"));
+
+        aggregated.forEach(row ->
+                System.out.printf(
+                        "%s=%2.1f/%2.1f/%2.1f\n",
+                        row.getString("Station"),
+                        row.getFloat("Min"),
+                        row.getDouble("Mean"),
+                        row.getFloat("Max")));
+    }
+}
